c23d6f7a4944ef1d4fd464ec821783be45959ea6
alexmr1 <alexmrobu@gmail.com>
2020-08-03 14:48:28 +0100

    Articles and comments schema created

---
+++ db/migrations/20200803143838_create_comments_table.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  console.log("creating comments table ...");
+  return knex.schema.createTable("comments", (commentsTable) => {
+    commentsTable.integer("comment_id").primary();
+    commentsTable.string("author").references("users.username");
+    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable.timestamp("created_at", { useTz: true });
+    commentsTable.text("body");
+  });
+};
+
+exports.down = function (knex) {
+  console.log("dropping comments table ...");
+  return knex.schemaDropTable("comments");
+};
