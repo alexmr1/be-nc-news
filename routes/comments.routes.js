dc8af4f49a9d503cb84b3536a9ac28556be10d78
alexmr1 <alexmrobu@gmail.com>
2020-08-07 12:14:33 +0100

    handle405s adde to sendAllComments

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -14,6 +14,6 @@ commentsRouter
   .delete(deleteComment)
   .all(handle405s);
 
-commentsRouter.route("/").get(sendAllComments);
+commentsRouter.route("/").get(sendAllComments).all(handle405s);
 
 module.exports = commentsRouter;
