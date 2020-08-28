92b37ecf204a53c411c0e32c0b93c129f56eafa6
alexmr1 <alexmrobu@gmail.com>
2020-08-28 12:47:57 +0100

    cors added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const apiRouter = require("./routes/api.routes");
+const cors = require("cors");
+
 const {
   handleCustomErrors,
   handlePsqlErrors,
@@ -7,6 +9,7 @@ const {
 } = require("./errors/index");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
